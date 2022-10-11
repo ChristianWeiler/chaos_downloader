5d4010b964c12b2d93b7594f510b55321143476f
ChristianWeiler <46946556+ChristianWeiler@users.noreply.github.com>
2022-10-11 00:57:55 +0000

    Rewrite

--- chaos_scraper.py
+++ chaos_scraper.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# https://chaos-data.projectdiscovery.io/index.json
-
 import requests
 import zipfile
 import io
@@ -10,86 +8,158 @@ from pathlib import Path
 import sys
 import argparse
 
-# Get the cwd
-script_path = Path.cwd()
+# json file containing public program information
+CHAOS_URL = "https://chaos-data.projectdiscovery.io/index.json"
 
-def is_up_to_date(target_path, last):
-    last_update = target_path / 'last_updated'
-    
-    # check if last_updated.txt exists
-    # if true, open and compare strings
-    if last_update.exists() != False:
-        with last_update.open('r') as f:
+# returns a list of publically avaialbe programs to scrape
+def get_programs():
+    return requests.get(url=CHAOS_URL).json()
+
+# downloads program zip file to program directory and extracts it
+# returns the directory the files were downloaded to
+def download_zip(program, url, download_dir):                    
+    r = requests.get(url, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(download_dir)
+
+# check if .last_updated exists if program directory
+# returns true if up-to-date or false if not
+def is_up_to_date(last_updated, chaos_last_updated):    
+    if last_updated.exists() == True:
+        with last_updated.open('r') as f:
             last_updated_file = f.read()
-            # return true if strings match
-            if last == last_updated_file:
-                return True
-            return False 
-    else:
-        # create last_updated.txt file with current value
-        with last_update.open('w') as f:
-            f.write(last)
+            return (chaos_last_updated == last_updated_file)
 
     return False
 
-# Download program zip file and extract it to the target directory
-def download_zip(target_path, url):
-    r = requests.get(url, stream=True)
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall(target_path)
+# takes a file path and prints out the subdomain
+# Ex: input: /tmp/.downloads/program/mysubdomain.com.txt output: mysubdomain.com
+def print_subdomain_from_path(subdomains):
+    for i in subdomains:
+        i = str(i).split('/')[-1]
+        i = i.split('.txt')[0]
+        print(i)
 
-def print_files(target_path):
-    
+# print all subdomains in each txt file in a program path
+def print_all_subdomains(path):
     subdomains = ""
-
-    # create a list of .txt files extracted from zip
-    for i in list(target_path.glob('*.txt')):
-        temp = target_path / i
-        
-        # open txt file, read and store contents
-        with temp.open('r') as f:
-            subdomains += f.read()
-
-    # print subdomains in txt files
-    print(subdomains)
-
-
-def urlparser(target):
-    # Get the index.json file which contains the download urls and parse it
-    index_parsed = requests.get(url='https://chaos-data.projectdiscovery.io/index.json').json()
-
-    # loop through parsed index.json
-    for i in index_parsed:
-        # replace spaces with underscores
-        target_name = i['name'].lower().replace(" ", "_")
-       
-        # check if you get a match for target name
-        if target.lower() in target_name:
-           
-            # change directories and check if directory exists
-            target_path = script_path / target_name
-           
-            if target_path.is_dir() != True:
-                target_path.mkdir()
-
-            # check if file is already up-to-date
-            if is_up_to_date(target_path, i['last_updated']):
-                return
+    # create a list of .txt files in the specified program directory
+    for i in list(path.glob('*.txt')):
+        temp = path / i
                     
-            # Download zip if not up-to-date
-            download_zip(target_path, i['URL'])
+    with temp.open('r') as f:
+        subdomains += f.read().strip()
 
-            print_files(target_path)
-
-    return
-
-
+    print(subdomains) 
+    
 def main():
-    parser = argparse.ArgumentParser(description='Search public subdomain informaiton on chaos.projectdiscovery.com')
-    parser.add_argument('program', type=str)
+
+    example_usage_text = """
+    Example:
+    python3 chaos_downloader.py --list-programs
+        4chan
+        84codes
+        8x8
+        Achmea
+        ...
+    python3 chaos_downloader.py --program dod --list-subdomains
+        health.mil
+        cac.mil
+        eucom.mil
+        dren.mil
+        ...
+    python3 chaos_downloader.py --program dod --subdomain health.mil,cac.mil,...
+        dha-101-.health.mil
+        dha-94-.health.mil
+        cpe-198-255-.health.mil
+        dha-227-.health.mil
+        ...
+    """
+
+    parser = argparse.ArgumentParser(description='Download public subdomain information from chaos.projectdiscovery.com',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog = example_usage_text)
+    parser.add_argument('--program','-p',help='Specify the program name to download data for', required=False, type=str)
+    parser.add_argument('--subdomain','-d',help='Restrict results to specific subdomains from program. Accepts multiple subdomains separated by commas', required=False, type=str)
+    parser.add_argument('--list-programs','-l',help='List all of the programs available to scrape and exits', required=False, action='store_true')
+    parser.add_argument('--list-subdomains','-s',help='List all of the subdomains available in a program and exits', required=False, action='store_true')
+    parser.add_argument('--verbose','-v',help='Don\'t print any output', required=False, action='store_true')
+    parser.add_argument('--force','-f',help='Ignore last_updated value and redownload program data', required=False, action='store_true')
     args = parser.parse_args()
 
-    urlparser(args.program)
+    # Setup arguments and program data
+    program_data = get_programs()
+    is_verbos = args.verbose        
+
+    # if --list flag is set, print a list of programs and exit
+    if args.list_programs == True:
+        for p in program_data:
+            print(p['name'])
+        sys.exit(0)
+
+    # check if --program name is specified
+    # if there is a program that matches, download data
+    if args.program is not None:
+        if is_verbos:
+            print(f"[*] Searching for program {args.program} ...")
+        
+        for p in program_data:
+            program_name = p['name'].lower().replace(" ", "_")
+
+            if args.program.lower() not in program_name :
+                continue
+
+            if is_verbos:
+                print(f"[+] Found program: {p['name']}")
+                print(f"[*] Program project chaos data was last updated: {p['last_updated']}")
+                
+            download_dir = Path.cwd() / ".downloads" / program_name
+            
+            # if program doesn't already have a folder, create it
+            if download_dir.is_dir() == False:
+                if is_verbos:
+                    print("[*] Program directory doesn't exist, creating new directory at %s", str(download_dir))
+                download_dir.mkdir(parents=True)
+
+            last_updated = download_dir / ".last_updated"
+
+            # only download program data if it is not already up-to-date, unless --force flag was used
+            if not is_up_to_date(last_updated, p['last_updated']) or args.force:
+                if is_verbos:
+                    print("[*] Program is not already up-to-date, downloading data ...")
+
+                # download and unzip program data to program directory
+                download_zip(program_name, p['URL'], download_dir)
+                
+                # update .last_updated file value
+                with last_updated.open('w') as f:
+                    f.write(p['last_updated'])
+
+            # get a list of all program subdomains from the .txt file names in the program directory
+            subdomains = list(download_dir.glob('*.txt'))
+
+            # if --list-subdomains was used, print the program subdomains and exit
+            if args.list_subdomains == True:
+                print_subdomain_from_path(subdomains)
+                sys.exit(0)
+
+            # if the --subdomain flag is set, only get the results from the specific subdomain
+            # else get all 
+            if args.subdomain is not None:
+                subdomain_args = args.subdomain.split(",")
+
+                for sub in subdomain_args:
+                    subdomain_file = sub + ".txt"
+                    subdomain_file = download_dir / subdomain_file
+                    
+                    subdomain_text = ""
+                    with subdomain_file.open('r') as f:
+                        subdomain_text = f.read().strip()
+
+                    print(subdomain_text)
+            else:
+                print_all_subdomains(download_dir)
+                   
 
 if __name__ == "__main__":
     main()
